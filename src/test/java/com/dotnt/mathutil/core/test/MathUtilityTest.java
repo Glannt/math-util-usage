2f03a1156532d04faebf7a5f7c4de1142b24d5b6
Glannt <tongnguyenthanhdo@gmail.com>
2024-10-19 13:08:55 +0700

    first commit

---
+++ src/test/java/com/dotnt/mathutil/core/test/MathUtilityTest.java
@@ -0,0 +1,70 @@
+package com.dotnt.mathutil.core.test;
+
+import org.dotnt.mathutil.core.MathUtility;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MathUtilityTest {
+    @Test
+    public void testFactoryThrowException() {
+        // Tra ve ngoai le nhu ki vong
+        // check ham tra ve co tra ve ngoai le nhu ki vong khong
+        // sai nhung nem ra ngoai le -> dung
+        assertThrows(IllegalArgumentException.class, () -> MathUtility.getFactorial(-5));
+        assertThrows(IllegalArgumentException.class, () -> {
+            MathUtility.getFactorial(-4);});
+        assertThrows(IllegalArgumentException.class, () -> {MathUtility.getFactorial(-2);});
+        assertThrows(IllegalArgumentException.class, () -> {MathUtility.getFactorial(-10);});
+        assertThrows(IllegalArgumentException.class, () -> {MathUtility.getFactorial(-15);});
+
+
+
+    }
+
+
+    //Testcase #4
+    @Test
+    public void testFactoryGivenWrongArgumentThrowException() {
+        // Tra ve ngoai le nhu ki vong
+        // check ham tra ve co tra ve ngoai le nhu ki vong khong
+        // sai nhung nem ra ngoai le dung
+        assertThrows(IllegalArgumentException.class, () -> {MathUtility.getFactorial(-5);});
+
+    }
+
+
+    //Testcase #3
+    @Test
+    public void testFactoryGivenRightArgumentReturnsWell(){
+        long expectedValue = 5;
+//        long actualValue = MathUtility.getFactorial(1);
+
+        // SO expeected va actual
+        assertEquals(1, MathUtility.getFactorial(1));
+        assertEquals(2, MathUtility.getFactorial(2));
+        assertEquals(6, MathUtility.getFactorial(3));
+    }
+
+
+    // Test case #2
+    @Test
+    public void testFactoryGivenRightArgument1ReturnsWell(){
+        long expectedValue = 1;
+//        long actualValue = MathUtility.getFactorial(1);
+
+        // SO expeected va actual
+        assertEquals(expectedValue, MathUtility.getFactorial(1));
+    }
+
+    // Test case #1:
+    @Test
+    public void testFactoryGivenRightArgument0ReturnsWell(){
+        long expectedValue = 1;
+        long actualValue = MathUtility.getFactorial(0);
+
+        // SO expeected va actual
+        assertEquals(expectedValue, actualValue);
+    }
+}
